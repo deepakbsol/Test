c359e5e563fc5199c515c932d313c828ee81aa99
Deepak kumar <deepak@bsol.com>
2021-05-06 16:07:37 +0530

    file-admin working

--- src/app/modules/home/modules/file-admin/service&route/file-admin-routing.module.ts
+++ src/app/modules/home/modules/file-admin/service&route/file-admin-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadedFileListComponent } from '../component/uploaded-file-list/uploaded-file-list.component';
 import { FileAdminComponent } from '../file-admin.component';
 import { FileAdminGuard } from './file-admin.guard';
 
 const routes: Routes = [
-  { path: '', component: FileAdminComponent,
-   resolve:{
-    data:FileAdminGuard
+
+  
+  { path: '', component: FileAdminComponent ,
+  children:[
+    { path: '', component: UploadedFileListComponent,
+      resolve:{
+            data:FileAdminGuard
+      }
     }
+  ]
   }
 ];
 
